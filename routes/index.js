6eb0b8c0a2ae3e12033c23aa30b6b4abf885bce0
binbin98 <36780979+binbin98@users.noreply.github.com>
2018-04-17 10:36:13 +0900

    タイトル変更ｆ

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: '新しいタイトルです！' });
+  res.render('index', { title: '新しいタイトルですf！' });
 });
 
 module.exports = router;
