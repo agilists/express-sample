79c6b34683a5a15f9781c448b8f4b51a2a984f68
m-sakakibara <46120752+m-sakakibara@users.noreply.github.com>
2018-12-25 15:04:14 +0900

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'title test 1' });
+  res.render('index', { title: 'title test 2' });
 });
 
 module.exports = router;
